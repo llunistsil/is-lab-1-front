43d6c811d67c1d3e229357dd85404812d4158507
llunsit <suvorovka04@gmail.com>
2024-10-30 19:13:29 +0300

    feat: add taiga-v4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,5 @@
+import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
@@ -5,8 +7,10 @@ import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-  ],
+    NG_EVENT_PLUGINS
+  ]
 };
